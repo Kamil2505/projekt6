49f26c5785cbb8e5ae89ff8fc4f94d5aaf10ce19
Kamil Lademann <kamil.lademann5@gmail.com>
2023-04-12 11:02:19 +0200

    zmiana kodu

--- signup.php
+++ signup.php
@@ -1,138 +1,139 @@
 <?php include('header.php'); ?>
 <?php
-$error=array("","","","","",true);
-if(isset($_POST['submit'])){
-    $imie=htmlspecialchars($_POST['imie']);
-    if ($imie == "" || strlen($imie) < 3){
-        $error[0] = "Brak Imienia";
-       // echo $error[0];
+$error = array("", "", "", "", "",true);
+if (isset($_POST['submit'])) {
+    $imie = htmlspecialchars($_POST['imie']);
+    if ($imie == "" || strlen($imie) < 3) {
+        $error[0] = "Brak imienia.";
+        // echo $error[0];
     }
-    $nazwisko=htmlspecialchars($_POST['nazwisko']);
+    $nazwisko = htmlspecialchars($_POST['nazwisko']);
     if ($nazwisko == "" || strlen($nazwisko) < 3) {
-        $error[1] = "Brak Nazwiska";
+        $error[1] = "Brak nazwiska.";
     }
-    $login=htmlspecialchars($_POST['login']);
-    if ($login == "" || strlen($login) < 3) {
-        $error[2] = "Brak Loginu";
+    $mail = htmlspecialchars($_POST['mail']);
+    if ($mail == "") {
+        $error[2] = "Brak mail'a.";
     }
-    $email=htmlspecialchars($_POST['email']);
-    if ($email == "") {
-        $error[3] = "Brak Emaila";
+    $login = htmlspecialchars($_POST['login']);
+    if ($login == "" || strlen($login) < 2) {
+        $error[3] = "Brak loginu.";
     }
-    $haslo1=htmlspecialchars($_POST['haslo1']);
-    $haslo2=htmlspecialchars($_POST['haslo2']);
-    if (!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/', $haslo1) 
-    ||  !preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/', $haslo2)
-    || $haslo1 != $haslo2
+    $haslo1 = htmlspecialchars($_POST['haslo1']);
+    $haslo2 = htmlspecialchars($_POST['haslo2']);
+    if (
+        !preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/', $haslo1)
+        || !preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/', $haslo2)
+        || $haslo1 != $haslo2
     )
-    $error[4] .= "Hasła musza m iec 8 znakow i byc identico";
-
-  
-            
-        $nazwisko= $_POST['nazwisko'];
-        $imie= $_POST['imie'];
-        $haslo1= $_POST['haslo1'];
-        $email= $_POST['email'];
-        $login= $_POST['login'];
-        $haslo2= $_POST['haslo2'];
-        // echo $nazwisko;
-        // echo $imie;
-        // echo $haslo1;
-        // echo $email;
-        // echo $login;
-        if(!empty($_POST['submit'])){
-            $error[5]=true;
-        }else {
-            $error[5]=false;
-        }
+        $error[4] = "Hasło musi posiadać min. 8 znaków. i być identyczne.";
+    // 1wwwdAw@dwaA!aa
+    
+    if (!empty(!empty(['regulamin']))) {
+        $error[5]=true;
+    } else {
+        $error[5]=false;
+    }
 }
-
 ?>
+
+<!-- formularz -->
+
 <div class="container">
     <div class="row">
         <div class="col-12">
             <!-- <form action="signup.php" method="post"> -->
             <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
-            <div class="mb-3">
-                <?php
-                if($error[0]!=""){
-
-                
-                ?>
-                <label class="alert alert-warning"><?php echo $error[0]; ?></label>
-<?php } ?>
-                <input type="text"id="imie" name="imie" placeholder="Imię" class="form-control" aria-labelledby="passwordHelpBlock">
+                <h2>formularz</h2>
                 <div class="mb-3">
-                <?php
-                if($error[1]!=""){
-
-                
-                ?>
-                <label class="alert alert-warning"><?php echo $error[1]; ?></label>
-<?php } ?>
-                <input type="text"id="nazwisko" name="nazwisko" placeholder="Nazwisko" class="form-control" aria-labelledby="passwordHelpBlock">
+                    <?php
+                    if ($error[0] != "") {
+                    ?>
+                        <label for="" class="alert alert-warning"><?php echo $error[0]; ?></label>
+                    <?php } ?>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Imię" name="imie">
+                </div>
                 <div class="mb-3">
-                <?php
-                if($error[2]!=""){
-
-                
-                ?>
-                <label class="alert alert-warning"><?php echo $error[2]; ?></label>
-<?php } ?>
-                <input type="text"id="login" placeholder="Login" name="login" class="form-control" aria-labelledby="passwordHelpBlock">
-                <label for="exampleFormControlInput1" class="form-label"></label>
+                    <?php
+                    if ($error[1] != "") {
+                    ?>
+                        <label for="" class="alert alert-warning"><?php echo $error[1]; ?></label>
+                    <?php } ?>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nazwisko" name="nazwisko">
+                </div>
                 <div class="mb-3">
-                <?php
-                if($error[3]!=""){
-
-                
-                ?>
-                <label class="alert alert-warning"><?php echo $error[3]; ?></label>
-<?php } ?>
-                <input type="email" class="form-control" name="email" id="exampleFormControlInput1" placeholder="name@example.com">
+                    <?php
+                    if ($error[3] != "") {
+                    ?>
+                        <label for="" class="alert alert-warning"><?php echo $error[3]; ?></label>
+                    <?php } ?>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="login" name="login">
+                </div>
                 <div class="mb-3">
-                <?php
-                if($error[4]!=""){
-
-                
-                ?>
-                <label class="alert alert-warning"><?php echo $error[4]; ?></label>
-<?php } ?>
-                <label for="inputPassword5" class="form-label">Password</label>
-                <input type="password" name="haslo1" id="inputPassword5" class="form-control" aria-labelledby="passwordHelpBlock">
-                <label for="inputPassword5" class="form-label">Password Again:</label>
-                <input type="password" id="inputPassword5" name="haslo2" class="form-control" aria-labelledby="passwordHelpBlock">
+                    <?php
+                    if ($error[2] != "") {
+                    ?>
+                        <label for="" class="alert alert-warning"><?php echo $error[2]; ?></label>
+                    <?php } ?>
+                    <input type="mail" class="form-control" id="exampleFormControlInput1" 
+placeholder="nazwa@mail.com
+" name="mail">
+                </div>
                 <div class="mb-3">
-                <?php
-                if($error[5]!=""){
-
-                
-                ?>
-                <label class="alert alert-warning">Musisz accpet avt regumalin</label>
-<?php } ?>
-                <input type="checkbox" value="Regulamin" name="submit"> Akceptuję Regulamin.<br>
-<input type="submit" value="Załóż konto" name="submit">
-<input type="reset" value="Wyczyść">
-
-
+                    <?php
+                    if ($error[4] != "") {
+                    ?>
+                        <label for="" class="alert alert-warning"><?php echo $error[4]; ?></label>
+                    <?php } ?>
+                    <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="hasło" name="haslo1">
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="powtórz hasło" name="haslo2">
+                </div>
+                <div class="form-check">
+                    <?php
+                    if(!$error[5]) {
+                        ?>
+                        <label for="" class="alert alert-warning">zaaceptuj regulamin</label>
+                    <?php } ?>
+                    <input class="form-check-input" type="checkbox" value="regulamin" id="flexCheckChecked" name="regulamin">
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Akceptuje regulamin strony Internetowej.
+                    </label>
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary mb-3" name="submit">Załóż konto</button>
+                    <button type="reset" class="btn btn-primary mb-3">Wyczyść</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
 
+<!-- end - formularz -->
 <?php
-if ($error[0] == "" && $error[1] == "" && $error[2] == "" && $error[3] == "" && $error[4] == "" && $error[5]) {
-    $conn = mysqli_connect('localhost', 'Josef', '123', 'portal');
-    if (!$conn) {
-        echo ' NIE POŁĄCZONO Z BAZĄ! Error: '.mysqli_connect_error() ; 
-    }else{
-    echo 'Połączono z bazą';
-    echo $_POST['imie'];
-    echo $_POST['nazwisko'];
-    echo $_POST['login'];
-    echo $_POST['email'];
-    echo $_POST['haslo1'];
-    echo $_POST['haslo2'];
-    echo $_POST['submit'];
-} 
+// if (preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/', '1wwwdAw@dwaA!aa'))
+//     echo "dobre hasło.";
+// else
+//     echo "błędne hasło.";
+if($error[0] == "" && $error[1] == "" && $error[2] == "" && $error[3] == "" && $error[4] == "" && $error[5] && isset($_POST['submit'])){
+    $conn = mysqli_connect('localhost', 'webPLA', '', 'Portal');
+        if(!$conn){
+            echo 'błąd połączenia z bazą danych. error : ' . mysqli_connect_error();
+        }
+        else{
+            echo 'połączono z bazą';
+            echo $_POST['imie'];
+            echo $_POST['nazwisko'];
+            echo $_POST['login'];
+            echo $_POST['mail'];
+            echo $_POST['haslo1'];
+            $datadodania = date("Y-m-d");
+            // zmienne 
+        $sql = "INSERT INTO users( imie, nazwisko, login, mail, haslo, regulamin, dataDodania)
+         VALUES ('$imie','$nazwisko','$login','$mail','$haslo1','true','$datadodania')";
+        mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        }
 }
-?>
+?> 
